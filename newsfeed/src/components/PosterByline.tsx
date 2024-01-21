4596971da664725db8d280ab4e534f9cce92ff48
Vladlan <lanvlad911@gmail.com>
2024-01-21 16:17:24 +0100

    Replace hardcoded ImageFragment

--- newsfeed/src/components/PosterByline.tsx
+++ newsfeed/src/components/PosterByline.tsx
@@ -9,7 +9,7 @@ const PosterBylineFragment  = graphql`
   fragment PosterBylineFragment on Actor {
       name
       profilePicture {
-        url
+        ...ImageFragment @arguments(width: 60, height: 60)
       }
     }
 `;

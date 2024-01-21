6eaa120f521e8346c02a66a01238ccd77975518d
Vladlan <lanvlad911@gmail.com>
2024-01-21 17:26:39 +0100

    Add organizationKind and location to PosterDetailsHovercardContentsBodyFragment

--- newsfeed/src/components/PosterDetailsHovercardContents.tsx
+++ newsfeed/src/components/PosterDetailsHovercardContents.tsx
@@ -3,15 +3,14 @@ import { useLazyLoadQuery, useFragment } from "react-relay";
 import { graphql } from "relay-runtime";
 import Image from "./Image";
 import Timestamp from "./Timestamp";
-import type {PreloadedQuery} from 'react-relay';
-import {usePreloadedQuery} from 'react-relay';
+import type { PreloadedQuery } from "react-relay";
+import { usePreloadedQuery } from "react-relay";
 import type { PosterDetailsHovercardContentsQuery as QueryType } from "./__generated__/PosterDetailsHovercardContentsQuery.graphql";
 import type { PosterDetailsHovercardContentsBodyFragment$key } from "./__generated__/PosterDetailsHovercardContentsBodyFragment.graphql";
+import OrganizationKind from './OrganizationKind';
 
 export const PosterDetailsHovercardContentsQuery = graphql`
-  query PosterDetailsHovercardContentsQuery(
-    $posterID: ID!
-  ) {
+  query PosterDetailsHovercardContentsQuery($posterID: ID!) {
     node(id: $posterID) {
       ... on Actor {
         ...PosterDetailsHovercardContentsBodyFragment
@@ -23,12 +22,9 @@ export const PosterDetailsHovercardContentsQuery = graphql`
 export default function PosterDetailsHovercardContents({
   queryRef,
 }: {
-  queryRef: PreloadedQuery<QueryType>,
+  queryRef: PreloadedQuery<QueryType>;
 }): React.ReactElement {
-  const data = usePreloadedQuery(
-    PosterDetailsHovercardContentsQuery,
-    queryRef,
-  );
+  const data = usePreloadedQuery(PosterDetailsHovercardContentsQuery, queryRef);
   return (
     <div className="posterHovercard">
       <PosterDetailsHovercardContentsBody poster={data.node} />
@@ -43,6 +39,14 @@ const PosterDetailsHovercardContentsBodyFragment = graphql`
     joined
     profilePicture {
       ...ImageFragment
+    }
+    ... on Organization {
+      organizationKind
+    }
+    ... on Person {
+      location {
+        name
+      }
     }
   }
 `;
@@ -66,11 +70,13 @@ function PosterDetailsHovercardContentsBody({
         <li>
           Joined <Timestamp time={data.joined} />
         </li>
+        {data.location != null && <li>{data.location.name}</li>}
+        {data.organizationKind != null && (
+          <li>
+            <OrganizationKind kind={data.organizationKind} />
+          </li>
+        )}
       </ul>
-      <div className="posterHovercard__buttons">
-        <button>Friend</button>
-        <button>Message</button>
-      </div>
     </>
   );
 }
